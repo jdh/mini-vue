c3451f7afa7f7e85a22fb5ecd66fefb039044c95
lehai <woloveni8@126.com>
2018-09-25 23:40:11 +0800

    剥离掉weex相关内容

---
+++ scripts/config.js
@@ -0,0 +1,109 @@
+const path = require('path')
+const buble = require('rollup-plugin-buble')
+const alias = require('rollup-plugin-alias')
+// const cjs = require('rollup-plugin-commonjs')
+const replace = require('rollup-plugin-replace')
+// const node = require('rollup-plugin-node-resolve')
+const flow = require('rollup-plugin-flow-no-whitespace')
+const version = process.env.VERSION || require('../package.json').version
+// const weexVersion = process.env.WEEX_VERSION || require('../packages/weex-vue-framework/package.json').version
+
+const banner =
+  '/*!\n' +
+  ' * Vue.js v' + version + '\n' +
+  ' * (c) 2014-' + new Date().getFullYear() + ' Evan You\n' +
+  ' * Released under the MIT License.\n' +
+  ' */'
+
+// const weexFactoryPlugin = {
+//   intro () {
+//     return 'module.exports = function weexFactory (exports, document) {'
+//   },
+//   outro () {
+//     return '}'
+//   }
+// }
+
+const aliases = require('./alias')
+const resolve = p => {
+  const base = p.split('/')[0]
+  if (aliases[base]) {
+    return path.resolve(aliases[base], p.slice(base.length + 1))
+  } else {
+    return path.resolve(__dirname, '../', p)
+  }
+}
+
+const builds = {
+  // Runtime+compiler development build (Browser)
+  'web-full-dev': {
+    entry: resolve('web/entry-runtime-with-compiler.js'),
+    dest: resolve('dist/vue.js'),
+    format: 'umd',
+    env: 'development',
+    alias: { he: './entity-decoder' },
+    banner
+  },
+  // Runtime+compiler production build  (Browser)
+  'web-full-prod': {
+    entry: resolve('web/entry-runtime-with-compiler.js'),
+    dest: resolve('dist/vue.min.js'),
+    format: 'umd',
+    env: 'production',
+    alias: { he: './entity-decoder' },
+    banner
+  },
+  // runtime-only build (Browser)
+  'web-runtime-dev': {
+    entry: resolve('web/entry-runtime.js'),
+    dest: resolve('dist/vue.runtime.js'),
+    format: 'umd',
+    env: 'development',
+    banner
+  }
+}
+
+function genConfig (name) {
+  const opts = builds[name]
+  const config = {
+    input: opts.entry,
+    external: opts.external,
+    sourceMap: true,
+    plugins: [
+      replace({
+        // __WEEX__: !!opts.weex,
+        // __WEEX_VERSION__: weexVersion,
+        __VERSION__: version
+      }),
+      flow(),
+      buble(),
+      alias(Object.assign({}, aliases, opts.alias))
+    ].concat(opts.plugins || []),
+    output: {
+      file: opts.dest,
+      format: opts.format,
+      banner: opts.banner,
+      name: opts.moduleName || 'Vue'
+    }
+  }
+
+  if (opts.env) {
+    config.plugins.push(replace({
+      'process.env.NODE_ENV': JSON.stringify(opts.env)
+    }))
+  }
+
+  Object.defineProperty(config, '_name', {
+    enumerable: false,
+    value: name
+  })
+
+  return config
+}
+
+if (process.env.TARGET) {
+  module.exports = genConfig(process.env.TARGET)
+} else {
+  exports.getBuild = genConfig
+  exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
+}
